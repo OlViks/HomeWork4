7e4376ccefbe82ca24b831cc5122d29076b51b2a
OlViks <salova.40@yandex.ru>
2022-09-15 23:50:32 +0300

    не понял правильность записи

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+       //Задание №1
+        byte classTable = 24;
+        short buttle = 200;
+        int board = 20000;
+        long screw = 1000000L;
+        float bolt = 3.24f;
+        double burger = 1.246;
+        System.out.println("Значение переменной classTable с типом byte равно " + classTable );
+        System.out.println("Значение переменной buttle с типом short равно " + buttle );
+        System.out.println("Значение переменной board с типом int равно " + board );
+        System.out.println("Значение переменной screw с типом long равно " + screw );
+        System.out.println("Значение переменной bolt с типом float равно " + bolt );
+        System.out.println("Значение переменной burger с типом double равно " + burger );
+    }
+}
