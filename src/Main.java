d35970b5ca9526e076dcae509a51b8458fdac2b2
OlViks <salova.40@yandex.ru>
2022-09-16 00:12:46 +0300

    не понял как записать  987 678 965 549

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,16 @@ public class Main {
         System.out.println("Значение переменной screw с типом long равно " + screw );
         System.out.println("Значение переменной bolt с типом float равно " + bolt );
         System.out.println("Значение переменной burger с типом double равно " + burger );
+        //Задния №2
+        double broad = 27.12;
+        long screwLong = 987678965549L;
+        byte table = 2;
+        short butlle = 786;
+        boolean b = 2 > 4;
+        short dog =  569;
+        short temperatur = -159;
+        short scool = 27897;
+        byte cat = 67;
+
     }
 }
